5e4d8847c861654361db84553835a2c2bf3e0f21
Andrew An <andyan7@gmail.com>
2022-01-17 15:08:06 -0800

    affixed .js to buttons properly

--- js/quiz.js
+++ js/quiz.js
@@ -1,24 +1,16 @@
-/*
- * questions.js is loaded in the HTML before quiz.js
- * It creates a global variable called questions that contains starter questions.
- * Take a look at the structure and familiarize yourself with each part
- * then, add some of your own questions!
- * Use this data to populate your quiz questions, choices, and answers.
- */
-
 // Global variables
-const questionNumber = 0;
-const gameContainer = document.querySelector('.game');
-const trivia = document.querySelector('.questions');
-const solutionList = document.querySelector('.solutions');
-const timerDiv = document.querySelector('.timer');
-const pScore = document.querySelector('.score');
-const button1 = document.querySelector('#one');
-const button2 = document.querySelector('#two');
-const button3 = document.querySelector('#three');
-const button4 = document.querySelector('#four');
-const POINTS = 5
-const mQuest = 5
+var questionNumber = 0;
+var gameContainer = document.querySelector('.game');
+var trivia = document.querySelector('.questions');
+var solutionList = document.querySelector('.solutions');
+var timerDiv = document.querySelector('.timer');
+var pScore = document.querySelector('.score');
+var button1 = document.querySelector('#one');
+var button2 = document.querySelector('#two');
+var button3 = document.querySelector('#three');
+var button4 = document.querySelector('#four');
+var POINTS = 5
+var mQuest = 5
 
 let currentQ = {}
 let acceptA = true
@@ -28,36 +20,36 @@ let questionsLeft = []
 var timer;
 var sec = 5;
 
-startGame = () => {
-    qCount = 0
-    score = 0
-    questionsLeft = [...questions]
-    getNewQ()
-}
+// startGame = () => {
+//     qCount = 0
+//     score = 0
+//     questionsLeft = [...questions]
+//     getNewQ()
+// }
 
-getNewQ = () => {
-    if (questionsLeft == 0 || qCount > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
+// getNewQ = () => {
+//     if (questionsLeft == 0 || qCount > MAX_QUESTIONS) {
+//         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/end.html')
-    }
+//         return window.location.assign('/end.html')
+//     }
 
-    qCount++
-    trivia.innerText = 'Question ${qCount} of ${MAX-QUESTIONS}'
+//     qCount++
+//     trivia.innerText = 'Question ${qCount} of ${MAX-QUESTIONS}'
 
-    const qIndex = Math.floor(Math.random() * questionsLeft.length)
-    currentQ = questionsLeft[questions]
-    questions.innerText = currentQ.question
+//     const qIndex = Math.floor(Math.random() * questionsLeft.length)
+//     currentQ = questionsLeft[questions]
+//     questions.innerText = currentQ.question
 
-    choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText = currentQ['choice' + number]
-    })
+//     choices.forEach(choice => {
+//         const number = choice.dataset['number']
+//         choice.innerText = currentQ['choice' + number]
+//     })
 
-    questionsLeft.splice(questionsIndex, 1)
+//     questionsLeft.splice(questionsIndex, 1)
 
-    acceptA = true
-}
+//     acceptA = true
+// }
 
 // choices.forEach(choice => {
 //     choice.addEventListener('click', e => {
@@ -105,10 +97,11 @@ function displayTrivia(i) {
     var trivia = document.querySelector('.questions');
 
     let triviaNew = '<span>' + questions[i].title + '</span>';
-    let solutionNew = '<button id="one">'+ questions[i].choices[0] +'</button>' + 
-    '<button id="two">' + questions[i].choices[1] + '</button>' + 
-    '<button id="three">' + questions[i].choices[2] + '</button>' + 
-    '<button id="four">' + questions[i].choices[3] + '</button>'
+    button1.textContent = questions[i].choices[0];
+    button2.textContent = questions[i].choices[1];
+    button3.textContent = questions[i].choices[2];
+    button4.textContent = questions[i].choices[3];
+
 
     trivia.innerHTML = triviaNew;
     solutionList.innerHTML = solutionNew;
@@ -133,11 +126,11 @@ startBtn.addEventListener('click',() => {
 
 // Check solution chosen by user
 function checkSol(i) {
-    // var playerChoice = questions[questionNumber].choices[i];
-    // var pSol = questions[questionNumber].answer;
+    var playerChoice = questions[questionNumber].choices[i];
+    var pSol = questions[questionNumber].answer;
 
-    // if (playerChoice != pSol) {
+    if (playerChoice != pSol) {
         
-    // }
-    console.log(i);
+    }
+
 }

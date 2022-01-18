ed2677a290af8971d5a68745270ab305ceb38eb6
Andrew An <andyan7@gmail.com>
2022-01-18 11:12:12 -0800

    Changes to css, .js, html

--- js/quiz.js
+++ js/quiz.js
@@ -1,18 +1,22 @@
 // Global variables
-var questionNumber = 0;
 var gameContainer = document.querySelector('.game');
 var trivia = document.querySelector('.questions');
 var solutionList = document.querySelector('.solutions');
-var solutionIndex = 0;
 var timerDiv = document.querySelector('.timer');
 var pScore = document.querySelector('.score');
 var button1 = document.querySelector('#one');
 var button2 = document.querySelector('#two');
 var button3 = document.querySelector('#three');
 var button4 = document.querySelector('#four');
+const startBtn = document.querySelector('#start');
+const endBtn = document.querySelector('#end')
+
+var solutionIndex = 0;
+var questionNumber = 0;
 var score = 0;
 var timer;
 var sec = 60;
+let userscore =0;
 
 // Timer function
 function timerF() {
@@ -20,21 +24,17 @@ function timerF() {
     timer = setInterval(function(){
         sec--;
         // console.log(sec);
-        timerDiv.textContent = ":" + sec;
+        timerDiv.textContent = "Left time:" + sec;
 
         if (sec <= 0) {
             clearInterval(timer);
         }
     }, 1000);
 }
-const startBtn = document.querySelector('#start');
-
-startBtn.addEventListener('click',() => {
-    timerF();
-});
 
 // Questions display
 function displayTrivia(i) {
+
     var trivia = document.querySelector('.questions');
 
     let triviaNew = '<span>' + questions[i].title + '</span>';
@@ -45,6 +45,56 @@ function displayTrivia(i) {
 
     trivia.innerHTML = triviaNew;
 }
+
+// Check solution chosen by user
+function checkSol(qns) {
+    var playerChoice = questions[questionNumber].choices[qns];
+    var pSol = questions[questionNumber].answer;
+    // for(var i=0; i < questions.length; i++) //we don't need it here
+
+    if (playerChoice === pSol) {
+        
+        score++;
+        pScore.innerHTML = 'Score: ' + score
+
+    } else {
+        score--;
+        sec-=1;
+    } 
+
+    if (questionNumber >= questions.length -1) {
+        endGame()
+    }
+    questionNumber++
+    displayTrivia(questionNumber)
+}
+
+function endGame(){
+    questionNumber = 0
+    sec=1 // 0 will be -1, so 1 will be 0 second
+    solutionList.classList.add('hide')
+
+    startBtn.classList.remove('hide')
+    endBtn.classList.add('hide')
+
+    startBtn.innerText = 'Restart'
+}
+
+startBtn.addEventListener('click',() => {
+    questionNumber = 0
+    sec = 60
+    score = 0
+
+    solutionList.classList.remove('hide')
+    endBtn.classList.remove('hide')
+    startBtn.classList.add('hide')
+
+    timerF();
+    displayTrivia(questionNumber);
+});
+
+// put eventlisten on the buttom
+endBtn.addEventListener('click', endGame)
 
 button1.addEventListener('click', () => {
     checkSol(0);
@@ -58,29 +108,3 @@ button3.addEventListener('click', () => {
 button4.addEventListener('click', () => {
     checkSol(3);
 });
-startBtn.addEventListener('click',() => {
-    displayTrivia(0);
-});
-
-
-// Check solution chosen by user
-function checkSol(qns) {
-    var playerChoice = questions[questionNumber].choices[qns];
-    var pSol = questions[questionNumber].answer;
-
-    for(var i=0; i < questions.length; i++)
-
-    if (playerChoice == pSol) {
-        
-        score++;
-
-    } else {
-        score--;
-        sec-=2;
-    } 
-    questionNumber++;
-    if (questionNumber < questions.length) {
-        displayTrivia(questionNumber);
-    }
-
-}
